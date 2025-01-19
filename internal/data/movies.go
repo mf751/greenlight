524808cf39f4cfac6d45bdb5e0d27e9dcd49af96
mf751 <mf751f@gmail.com>
2025-01-20 00:18:11 +0300

    update movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -85,7 +85,21 @@ WHERE id = $1;
 }
 
 func (model *MovieModel) Update(movie *Movie) error {
-	return nil
+	sqlStatement := `
+UPDATE movies
+SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+WHERE id = $5
+RETURNING version
+  `
+	args := []interface{}{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return model.DB.QueryRow(sqlStatement, args...).Scan(&movie.Version)
 }
 
 func (model *MovieModel) Delete(id int64) error {
