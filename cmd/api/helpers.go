da4b0a5529098c1e10ce9fd8c116c0314da87cde
mf751 <mf751f@gmail.com>
2025-01-29 17:12:32 +0300

    background tasks gracefull shutdown

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -150,7 +150,9 @@ func (app *application) readInt(
 }
 
 func (app *application) background(function func()) {
+	app.wg.Add(1)
 	go func() {
+		defer app.wg.Done()
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.PrintError(fmt.Errorf("%s", err), nil)
--- cmd/api/server.go
+++ cmd/api/server.go
@@ -37,7 +37,17 @@ func (app *application) serve() error {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
-		shutdownError <- srv.Shutdown(ctx)
+		err := srv.Shutdown(ctx)
+		if err != nil {
+			shutdownError <- err
+		}
+
+		app.logger.PrintInfo("completeing background tasks", map[string]string{
+			"Addr": srv.Addr,
+		})
+
+		app.wg.Wait()
+		shutdownError <- nil
 	}()
 
 	app.logger.PrintInfo("starting server", map[string]string{
